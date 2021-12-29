7e4ab6bbe123f40d0b85d8669f64a803e0a0164d
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-12-30 03:59:48 +0900

    [Feat] 로그인 라우터 준비

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,5 +3,5 @@ import express from "express";
 const userRouter = express.Router();
 
 userRouter.route("/").post((req, res) => res.send("여기는 회원가입"));
-
+userRouter.route("/login").post((req, res) => res.send("여기는 로컬 로그인"));
 export default userRouter;
