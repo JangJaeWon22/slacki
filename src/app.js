e324fc1bd5a68669eacc48a36274293affed0ffd
Yeon Jeffrey <yeonjeseo@hotmail.com>
2022-01-05 18:15:52 +0900

    [Comment] md 단어 교정, log 삭제

--- src/app.js
+++ src/app.js
@@ -21,6 +21,5 @@ db.sequelize
 // 여기서 uri 시작에 /를 붙였어야 함!
 app.use("/api/users", userRouter);
 // app.get("/api/test", (req, res) => res.send("Hello world!"));
-console.log("????");
 
 app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
