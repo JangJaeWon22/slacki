9f86d70c9448fb022ddbbe186de876c238045041
jjangjae <nick2866@naver.com>
2022-01-07 22:13:18 +0900

    Feat ESlint, prettier 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 // .eslintrc.js
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
     // Airbnb style guide 적용
-    "airbnb-base",
+    'airbnb-base',
     // TypeScript ESLint recommanded style 적용
-    "plugin:@typescript-eslint/eslint-recommended",
+    'plugin:@typescript-eslint/eslint-recommended',
   ],
   rules: {
     // ESLint 기본적으로 console 허용 안 함.
-    "no-console": 0,
+    'no-console': 0,
   },
 };
