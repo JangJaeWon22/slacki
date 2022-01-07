c175697f590a585da47ef23f4656d5d0f4d7689b
jjangjae <nick2866@naver.com>
2022-01-08 01:21:34 +0900

    [fix] 기본 ts구조 형태 변경 및 ts 오류 해결

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,30 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   extends: [
     // Airbnb style guide 적용
-    'airbnb-base',
+    "airbnb-typescript/base",
     'plugin:node/recommended',
     'prettier',
     // TypeScript ESLint recommanded style 적용
-    'plugin:@typescript-eslint/eslint-recommended',
+    // 'plugin:@typescript-eslint/eslint-recommended',
   ],
   rules: {
     // ESLint 기본적으로 console 허용 안 함.
-    'no-console': 0,
-    'import/prefer-default-export': 'off',
+    "no-console": "error",
+    "import/prefer-default-export": "off",
+    "unicorn/no-array-reduce": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "node/no-unsupported-features/es-syntax": [
+      "error",
+      { ignores: ["modules"] },
+    ],
     'import/no-unresolved': [
       'error',
-      { caseSensitive: false },
-    ],
+      { "caseSensitive": false }
+    ],  
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {} // this loads <rootdir>/tsconfig.json to eslint
+    },
   },
 };
---
+++ src/routers/userRouter.ts
@@ -0,0 +1,16 @@
+import express,{ Request, Response} from "express";
+// import {
+//   deleteUser,
+//   patchUser,
+//   postUsers,
+// } from '../controllers/userController';
+
+const userRouter = express.Router();
+
+// userRouter.route('/').post(postUsers);
+// userRouter.route('/login').post((req: Request, res: Response) => res.send('여기는 로컬 로그인'));
+// userRouter.route('/:userId').delete(deleteUser).patch(patchUser);
+/*
+  로그아웃은 프론트엔드에서 localStorage 토큰 삭제로 구현 예정
+*/
+export default userRouter;
